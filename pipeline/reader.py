227b99dfec56dfdb4fb30fb0a79c0198859e2436
BachNg <ngbach06@gmail.com>
2022-09-18 09:46:23 +0700

    Modify for vietnamese

--- pipeline/reader.py
+++ pipeline/reader.py
@@ -91,7 +91,8 @@ class Reader:
             max_query_length=args.max_query_length,
             is_training=False,
             quiet = True)
-
+        for f in features:
+            print(f.input_ids)
         all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
         all_input_masks = torch.tensor([f.input_mask for f in features], dtype=torch.long)
         all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
@@ -114,6 +115,7 @@ class Reader:
                 start_logits = batch_start_logits[i].detach().cpu().tolist()
                 end_logits = batch_end_logits[i].detach().cpu().tolist()
                 switch_logits = batch_switch_logits[i].detach().cpu().tolist()
+                print('sssssssss', switch_logits)
                 eval_feature = features[f_offset+i]
                 unique_id = int(features[f_offset+i].unique_id)
                 all_results.append(RawResult(unique_id=unique_id,
