1e1b15f4a0a8a5e219d5169db816c901b9fb0f79
BachNg <ngbach06@gmail.com>
2022-09-19 09:03:58 +0700

    Modify for vietnamese

--- reader/rc_utils.py
+++ reader/rc_utils.py
@@ -1018,7 +1018,7 @@ def write_predictions_yes_no_beam(all_examples, all_features, all_results, n_bes
             prelim_predictions,
             key=lambda x: (x.start_logit + x.end_logit),
             reverse=True)
-        print('CCCCCCCCCCCC',example.para_titles, prelim_predictions)
+        print('CCCCCCCCCCCC',  example.qas_id,example.para_titles, prelim_predictions)
         _NbestPrediction = collections.namedtuple(  # pylint: disable=invalid-name
             "NbestPrediction", ["text", "start_logit", "end_logit", "no_answer_logit", "switch", "switch_logits"])
         no_answer_logit = score_null
